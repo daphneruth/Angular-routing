91575148092e1f39c2a840451ad05d57e57a0df8
daphneruth <daphneruth304@gmail.com>
2023-07-25 16:51:10 +0300

    added more components

---
+++ src/app/modules/admin/admin-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './component/about/about.component';
+import { AdminDashboardComponent } from './component/admin-dashboard/admin-dashboard.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { HomeComponent } from './component/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminDashboardComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+    
+      { path: 'contact', component: ContactComponent },
+      { path: '', redirectTo: '/admin/home', pathMatch: 'full' },
+    ],
+  },
+];
+
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AdminRoutingModule { }
---
+++ src/app/modules/admin/admin.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { HeaderComponent } from './component/header/header.component';
+import { AboutComponent } from './component/about/about.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { HomeComponent } from './component/home/home.component';
+import { AdminDashboardComponent } from './component/admin-dashboard/admin-dashboard.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    AboutComponent,
+    FooterComponent,
+    ContactComponent,
+    HomeComponent,
+    AdminDashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule { }
