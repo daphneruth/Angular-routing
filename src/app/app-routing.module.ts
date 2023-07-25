022e11f3d031bd907101a4b29888348e1310fda3
daphneruth <daphneruth304@gmail.com>
2023-07-25 16:52:27 +0300

    routed new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './modules/admin/component/about/about.component';
+import { AdminDashboardComponent } from './modules/admin/component/admin-dashboard/admin-dashboard.component';
+import { ContactComponent } from './modules/admin/component/contact/contact.component';
+import { HomeComponent } from './modules/admin/component/home/home.component';
 
 const routes: Routes = [
-
+  {
+    path: '',
+    component: AdminDashboardComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      
+      { path: 'contact', component: ContactComponent },
+      { path: '', redirectTo: '/admin/home', pathMatch: 'full' },
+    ],
+  },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
