dce79b400741dd7a7b1990a1f1301e3ea87b6b33
daphneruth <daphneruth304@gmail.com>
2023-07-25 13:15:59 +0300

    routed the components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'anular-routing'`, () => {
+  it(`should have as title 'angular-routing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('anular-routing');
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('anular-routing app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-routing app is running!');
   });
 });
